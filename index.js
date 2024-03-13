f859621efcb58ad3c603f19424b8de487aa49394
Aleksandr Kubenko <151834458+Gumeynik@users.noreply.github.com>
2024-03-13 19:55:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,105 +1,107 @@
-// const person = {
-//     name: 'Sawa',
-//     surname: 'kubenko',
-//     age: 25,
-//     city: 'moscow',
-//     getFullName: function() {
-//         return `${this.name} ${this.surname}`;
-//     },
-//     isAdult: function () {
-//         return this.age > 18
-//     }
-// }
-// console.log(person.getFullName())
-// console.log(person)
+const posts = [];
+const date = getUserTime(new Date());
+const titleLimit = 100;
+const textLimit = 200;
 
 
+const inputTitleNode = document.querySelector('.js-input-title');
+const inputTextNode = document.querySelector('.js-input-text');
+const btnNode = document.querySelector('.js-btn');
+const postsNode = document.querySelector('.js-posts');
+const validationMessage = document.getElementById('validationMessage')
 
+btnNode.addEventListener('click', function () {
+    const postFromUser = getPostFromUser();
 
-// const people = [ 
-//     {
-//         name: 'Sawa',
-//         age: 25,
-//         city: 'moscow',
-//     },
-//     {
-//         name: 'petya',
-//         age: 22,
-//         city: 'moscow',
-//     },
-//     {
-//         name: 'Max',
-//         age: 23,
-//         city: 'sychi',
-//     },]
-// people.forEach(man => {
-//     console.log(man.name, man.age, man.city)
-// });
-// function printNumbers() {
-//  for (let index = -10; index <= 10; index++) {
-//     console.log(index)
-//  }  
-// }
-// printNumbers()
+    addPost(postFromUser);
 
-// function printNumbersByRange(start, end){
-//  for (let index = start; index <= end; index++) {
-//     console.log(index)
-//  }  
-// }
-// printNumbersByRange(-3, 2)
+    renderPosts();
+});
 
-// function calculateNumbersSum(numbers){
-//     let sum = 0;
-//     numbers.forEach(function(number) {
-//             sum = sum + number;
-//     });
-//     return sum
-// }
-// console.log(calculateNumbersSum([-10, 1, 2, 4, 9]))
-
-
-// function includes(numbers, number) {
-//     for (let i = 0; i < numbers.length; i++) {
-//         if (numbers[i] === number) {
-//             return true;
-//         } 
-//     }
-//     return false;
-// }
-
-// console.log(includes([1, 2, 3, 4, 5], 2));
-
-// function  createPerson(name, surname, age) {
-//     const person = {
-//         name: name,
-//         surname: surname,
-//         age: age,
-//     }
-//     return person;
-// }
-// console.log(createPerson('sawa', 'kubenko', 25))
-function createPerson(name, age) {
-    const person = {
-        name: name,
-        age: age,
-    }
-    return person
+inputTitleNode.addEventListener('input', function(){
+    validation();
 }
-function findOldest(people) {
-    let oldestPerson = 0;
-    people.forEach(function (person, index) {
-        if (person.age > people[oldestPerson].age) {
-            oldestPerson = index;
-        }
-    }
-    );
+);
 
-    return people[oldestPerson].name
+inputTextNode.addEventListener('input', function () {
+        validation();
+});
+
+
+function validation() {
+    const titleLength = inputTitleNode.value.length;
+    const textLength = inputTextNode.value.length;
+
+     if (titleLength > titleLimit) {
+        validationMessage.innerText = `Максимум символов ${titleLimit}. Лимит символов превышен на ${titleLength - titleLimit} .`;
+        validationMessage.classList.remove('validationMessage_hidden');
+        btnNode.setAttribute('disabled', true)
+        return;
+    } if (textLength > textLimit) {
+        validationMessage.innerText = `Максимум символов ${textLimit}. Лимит символов превышен на ${textLength - textLimit} .`;
+        validationMessage.classList.remove('validationMessage_hidden');
+        btnNode.setAttribute('disabled', true)
+        return;
+    } else {
+        validationMessage.classList.add('validationMessage_hidden');
+        btnNode.removeAttribute('disabled');
+        return;
+    }
 }
-const people = [
-    createPerson('Qnach', 320),
-    createPerson('Chromov', 65),
-    createPerson('Sitnikov', 55)
-];
-console.log(findOldest(people))
+
+
+
+function getPostFromUser() {
+   const title = inputTitleNode.value;
+   const text = inputTextNode.value;
+
+    return {
+        title: title,
+        text: text,
+    };
+};
+
+function addPost({title, text}) {
+    const currentDate = getUserTime(new Date()); 
+    posts.push({
+     title,
+     text,
+     date: currentDate,
+    });
+ }
+;
+
+function getPosts() {
+    return posts;
+}
+
+function renderPosts() {
+    const posts = getPosts();
+
+    let postsHTML = '';
+
+    posts.forEach(post => {
+        postsHTML += `
+            <div class='post'>
+                <div class='post__date'>${post.date}</div>
+                 <p class='post__title'>${post.title} </p>
+                <textarea class='post__text'>${post.text} </textarea>
+            </div>
+            `;
+    });
+
+    postsNode.innerHTML = postsHTML; 
+}
+
+
+function addLeadingZero(d) {
+  return ( d < 10) ? '0' + d : d;
+}
+function getUserTime(t) {
+    let Y = t.getFullYear();
+    let M = addLeadingZero(t.getMonth() + 1);
+    let D = addLeadingZero(t.getDate());
+    let H = addLeadingZero(t.getHours());
+    let m = addLeadingZero(t.getMinutes());
+    return `${D}.${M}.${Y} ${H}:${m}` 
+}
